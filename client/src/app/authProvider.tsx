0d01ad9a176b66c6e5234a41921041132bd4e69e
ed-roh <edwardroh89@gmail.com>
2024-08-21 18:01:09 -0700

    css styling final changes

--- client/src/app/authProvider.tsx
+++ client/src/app/authProvider.tsx
@@ -44,7 +44,7 @@ const formFields = {
 
 const AuthProvider = ({ children }: any) => {
   return (
-    <div className="mt-5">
+    <div>
       <Authenticator formFields={formFields}>
         {({ user }: any) =>
           user ? (
