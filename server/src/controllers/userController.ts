1c18f8c555f5be33201ab7703c7a3555d49ba4f6
ed-roh <edwardroh89@gmail.com>
2024-08-21 17:01:03 -0700

    create user route post trigger

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -13,3 +13,27 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
       .json({ message: `Error retrieving users: ${error.message}` });
   }
 };
+
+export const postUser = async (req: Request, res: Response) => {
+  try {
+    const {
+      username,
+      cognitoId,
+      profilePictureUrl = "i1.jpg",
+      teamId = 1,
+    } = req.body;
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        cognitoId,
+        profilePictureUrl,
+        teamId,
+      },
+    });
+    res.json({ message: "User Created Successfully", newUser });
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error retrieving users: ${error.message}` });
+  }
+};
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { getUsers } from "../controllers/userController";
+import { getUsers, postUser } from "../controllers/userController";
 
 const router = Router();
 
 router.get("/", getUsers);
+router.post("/", postUser);
 
 export default router;
